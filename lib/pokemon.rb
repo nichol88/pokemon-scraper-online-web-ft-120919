5a76785bc54a7e8e75043e5040f2da9404f990ff
Brian Nicholls <nichol88@gmail.com>
2020-01-22 20:34:48 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -21,7 +21,7 @@ class Pokemon
     sql = <<-SQL
     SELECT * FROM pokemon WHERE id = ?
     SQL
-    
+
     row = db.execute(sql, id)[0]
 
     Pokemon.new(id: row[0], name: row[1], type: row[2])
